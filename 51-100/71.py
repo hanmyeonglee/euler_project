04aa006e8797c9b420fd1b999718d42c2de2f7d5
Hann <ejongwon7@gmail.com>
2025-04-19 11:52:58 +0900

    prob.71

--- 51-100/71.py
+++ 51-100/71.py
@@ -1,26 +1,11 @@
-from math import gcd
+from math import floor
 
+num = 0
+den = 1
+for d in range(2, 1_000_001):
+    n = floor(3 * d / 7)
+    if num * d < den * n and 7 * n < 3 * d:
+        num = n
+        den = d
 
-def make_fractions(n: int) -> list[float]:
-    ret = []
-    for i in range(1, n):
-        if gcd(n, i) == 1:
-            ret.append((i / n, i))
-    return ret
-
-
-def main() -> None:
-    all_fractions = []
-    for n in range(2, 1000001):
-        all_fractions.extend(make_fractions(n))
-        if n % 10000 == 0:
-            print(f'{n = }')
-
-    all_fractions.sort(key=lambda x: x[0])
-
-    ind = all_fractions.index((3/7, 3))
-    print(all_fractions[ind-1][1])
-
-
-if __name__ == "__main__":
-    main()
+print(num)
